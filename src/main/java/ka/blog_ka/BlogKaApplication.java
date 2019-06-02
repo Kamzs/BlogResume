5d914beacc96ebb0963a69e5b0ef82fa72a9eec9
Kamzs <kamil.ambroziak.priv@gmail.com>
2019-06-02 13:49:59 +0200

    start

---
+++ src/main/java/ka/blog_ka/BlogKaApplication.java
@@ -0,0 +1,13 @@
+package ka.blog_ka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogKaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogKaApplication.class, args);
+	}
+
+}
