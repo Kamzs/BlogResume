95d57a6215a76443188674d314eb0e20f90f4048
Kamzs <kamil.ambroziak.priv@gmail.com>
2019-06-02 22:12:53 +0200

    beta version

---
+++ src/main/java/ka/blog_ka/PagesController.java
@@ -0,0 +1,26 @@
+package ka.blog_ka;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagesController
+{
+
+    @GetMapping("/")
+    public String getMainPage(){return "index";}
+
+    @GetMapping("/technologies")
+    public String getTechnologiesPage(){return "technologies";}
+
+    @GetMapping("/education")
+    public String getEducationPage(){return "education";}
+
+    @GetMapping("/experiance")
+    public String getExperiancePage(){return "experiance";}
+
+    @GetMapping("/hobbys")
+    public String getHobbysPage(){return "hobbys";}
+
+
+}
